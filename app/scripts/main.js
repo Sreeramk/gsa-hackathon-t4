0d088192b12a7521aa80d36e72445f577eeff481
Juliet Sabol <julietsabol@gmail.com>
2015-05-08 11:55:20 -0400

    add carto map and page stucture

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,3 @@
-/* jshint devel:true */
-console.log('\'Allo \'Allo!');
+window.onload = function() {
+  cartodb.createVis('map', 'http://documentation.cartodb.com/api/v2/viz/2b13c956-e7c1-11e2-806b-5404a6a683d5/viz.json');
+}
